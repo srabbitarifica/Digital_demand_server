d15ffb5336837cb8486383e43c05e702faac007b
madhutarifica <mpaudel@tarifica.com>
2023-08-23 16:36:17 -0400

    feat: sync the code

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,55 @@
-// index.tsx
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import viewDigitalData from "./routes/viewDigitalData.js";
+import signupRoute from "./routes/signupRoute.js";
+import loginRoute from "./routes/loginRoute.js";
+
+import bodyParser from "body-parser";
+import mysql from "mysql2";
 
 const app = express();
 
-// Enable CORS for all routes
-app.use(cors(process.env.LocalUrl));
+const corsOrigin = {
+  origin: "http://localhost:3000",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOrigin));
 
 dotenv.config();
+
+app.use(bodyParser.json());
+
+export const pool = mysql.createPool({
+  host:  process.env.DIGITAL_DB_HOST,
+  user: process.env.DIGITAL_DB_USERNAME,
+  password: process.env.DIGITAL_DB_PASSWORD,
+  database: process.env.DIGITAL_DB_DATABASE,
+  port: parseInt(process.env.DIGITAL_DB_SQLPORT)
+});
+
+pool.getConnection((err, connection) => {
+  if (err) {
+    console.error("Error connecting to MySQL:", err);
+    return;
+  }
+  console.log("Connected to MySQL database!");
+
+  connection.release();
+});
 
 app.get("/", (req, res) => {
   res.send("Hello, World");
 });
 
-// API endpoint to read data from CSV file and send in response
 app.get("/data", viewDigitalData);
 
-// Start the server
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on http://localhost:${process.env.PORT}`);
+app.post("/login", loginRoute);
+
+app.post("/signup", signupRoute);
+
+app.listen(process.env.BACKEND_PORT, () => {
+  console.log(`Server is running on http://localhost:${process.env.BACKEND_PORT}`);
 });
